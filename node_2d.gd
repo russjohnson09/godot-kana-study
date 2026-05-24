410114b492d22ec74b96aeab22daaf5dcb68435f
Russell Johnson <russjohnson09@gmail.com>
2026-05-24 18:05:56 -0400

    update

--- node_2d.gd
+++ node_2d.gd
@@ -6,3 +6,8 @@ extends Node2D
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+
+func _on_timer_timeout() -> void:
+	$Intro.play()
+	pass # Replace with function body.
